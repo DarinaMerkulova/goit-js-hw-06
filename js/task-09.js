2ccd97a5ee314a91ecc78f499ecd8f352a26c573
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-04-18 22:32:29 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,15 @@
-const btnChangeColor = document.querySelector(".change-color")
-let colorName = document.querySelector(".color")
+const btnChangeColor = document.querySelector(".change-color");
+let colorName = document.querySelector(".color");
 
-btnChangeColor.addEventListener("click", changeColorBackground)  
-  
+btnChangeColor.addEventListener("click", changeColorBackground);
 
-function changeColorBackground () 
-{document.body.style.backgroundColor = getRandomHexColor();
-  colorName.textContent = document.body.style.backgroundColor}
-
-
+function changeColorBackground() {
+  colorName.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorName.textContent;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
